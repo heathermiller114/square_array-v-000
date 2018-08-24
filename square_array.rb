cc3798b8a8e1ff7766212bd014aedf2c63edc859
Heather Miller <heather.miller114@gmail.com>
2018-08-24 02:38:22 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
-  squared = []
-  array.each do |number|
-    squared << number ** 2
-  end  
-  squared  
-end
+#def square_array(array)
+  #squared = []
+  #array.each do |number|
+    #squared << number ** 2
+  #end  
+  #squared  
+#end
 
 def square_array(array)
   array.collect{ |number| number ** 2 }
